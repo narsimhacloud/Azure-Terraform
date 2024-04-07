5c59a72bf94b5f7d44a0b310e314fdc4e4e10d3f
narsimhacloud <166274054+narsimhacloud@users.noreply.github.com>
2024-04-07 01:19:22 -0500

    Create variables.tf

---
+++ monitor/AppInsights/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  description = "The name of the Azure resource group."
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be deployed."
+}
