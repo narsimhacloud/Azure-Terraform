9af7d3f9dde0bef3c93cccfbc9924622a10dd3a1
narsimhacloud <166274054+narsimhacloud@users.noreply.github.com>
2024-04-07 01:15:54 -0500

    Create variables.tf

---
+++ networking/azure_vpn_gateway/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the Azure resource group."
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be deployed."
+}
+
+variable "virtual_network_name" {
+  description = "The name of the virtual network."
+}
