4f0b4b0457c593f4bf0e0357a592f9adca673933
narsimhacloud <166274054+narsimhacloud@users.noreply.github.com>
2024-04-06 23:56:16 -0500

    Update provider.tf

--- vm/provider.tf
+++ vm/provider.tf
@@ -3,7 +3,7 @@ az login
 
 az account list
 
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/c40c3148-7e94-43d9-86d4-772006e9136a"
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/c40c3-86d4-772006e9136a"
 */
 
 
@@ -22,8 +22,8 @@ provider "azurerm" {
   alias = "backend"  # Add an alias to differentiate this configuration
   features {}
 
-  subscription_id = "c40c3148-7e94-43d9-86d4-772006e9136a"
-  client_id       = "1f8045fb-d487-4479-9164-3a0f5ae6a828"
-  client_secret   = "jWB8Q~1s4~-jrkfIG2oFmHkoHCP_PVYr21hbqb2e"
-  tenant_id       = "0df65618-1067-4395-9adf-4f63145d2a83"
+  subscription_id = "c40c3148-7e6d4-772006e9136a"
+  client_id       = "1f8045fb-d487-4a0f5ae6a828"
+  client_secret   = "jWB8Q~1s4~-jrkfIG2oFmYr21hbqb2e"
+  tenant_id       = "0df65615-9adf-4f63145d2a83"
 }
